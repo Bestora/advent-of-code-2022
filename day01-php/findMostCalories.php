34dc699045d742981ad0d55692a0ca6289e10825
Dennis Rehbehn <dennisrehbehn@googlemail.com>
2023-11-13 19:52:45 +0100

    Creating simple and clean day 1 solution with php

---
+++ day01-php/findMostCalories.php
@@ -0,0 +1,11 @@
+<?php
+if($argc != 2) die("Missing input file");
+
+$input = file_get_contents($argv[1]);
+$input = str_replace("\r", "", $input); // fixing windows
+$elfes = explode("\n\n", $input);
+foreach($elfes as &$elv) {
+    $calories = explode("\n", $elv);
+    $elv = array_sum($calories);
+}
+echo max($elfes);
